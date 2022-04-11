221aa73314ff262fb8b3146454928cce553321cb
Nikita Makarand Pande <53806378+niki1312@users.noreply.github.com>
2022-04-11 11:09:59 +0530

    Update Q1SqlBasic.sql

--- Q1SqlBasic.sql
+++ Q1SqlBasic.sql
@@ -30,8 +30,9 @@ id  |   Name		|CityCode|  District	|Population
 select * from city
 where countrycode='USA' and population>100000
 
-3878 Scottsdale USA Arizona 202705 
-3965 Corona USA California 124966 
-3973 Concord USA California 121780 
-3977 Cedar Rapids USA Iowa 120758 
-3982 Coral Springs USA Florida 117549 
+id  |   Name		|CityCode|  District	|Population
+3878| Scottsdale        | USA    |  Arizona     | 202705 
+3965| Corona            | USA    |  California  | 124966 
+3973| Concord           | USA    |  California  | 121780 
+3977| Cedar Rapids      | USA    |  Iowa        | 120758 
+3982| Coral Springs     | USA    |  Florida     | 117549 
