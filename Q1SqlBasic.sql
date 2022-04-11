6fb4fd047f2032fd9348e15753afd36048231f7c
Nikita Makarand Pande <53806378+niki1312@users.noreply.github.com>
2022-04-11 10:58:04 +0530

    Update Q1SqlBasic.sql

--- Q1SqlBasic.sql
+++ Q1SqlBasic.sql
@@ -4,6 +4,7 @@ use practice
 --create table
 create table city(ID int,Name varchar(17),Countrycode varchar(3),district varchar(20),population int)
 
+
 select * from city
 
 --insert values into city table
@@ -14,6 +15,12 @@ insert into city values(3878,'Scottsdale', 'USA', 'Arizona', 202705),
                         (3982, 'Coral Springs', 'USA', 'Florida', 117549)
 
 select * from city
+ID	     Name	       Countrycode	   district	  population
+3878	Scottsdale	    USA	           Arizona	   202705
+3965	 Corona	        USA	          California	 124966
+3973	 Concord	      USA	          California	 121780
+3977	 Cedar Rapids	  USA              Iowa	     120758
+3982	 Coral Springs  USA	           Florida	   117549
 
 --question
 --Query all columns for all American cities in the CITY table with populations larger than 100000. The CountryCode for America is USA.
@@ -21,3 +28,9 @@ select * from city
 select * from city
 where countrycode='USA' and population>100000
 
+ID	     Name	       Countrycode	   district	  population
+3878	Scottsdale	    USA	           Arizona	   202705
+3965	 Corona	        USA	          California	 124966
+3973	 Concord	      USA	          California	 121780
+3977	 Cedar Rapids	  USA              Iowa	     120758
+3982	 Coral Springs  USA	           Florida	   117549
