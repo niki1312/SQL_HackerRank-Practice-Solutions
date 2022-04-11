b17bda01d549cb1d674a533a6be2a5c608b3332a
Nikita Makarand Pande <53806378+niki1312@users.noreply.github.com>
2022-04-11 11:09:41 +0530

    Update Q1SqlBasic.sql

--- Q1SqlBasic.sql
+++ Q1SqlBasic.sql
@@ -17,12 +17,12 @@ insert into city values(3878,'Scottsdale', 'USA', 'Arizona', 202705),
 select * from city
 
 				
-				
-3878| Scottsdale        | USA  |  Arizona     | 202705 
-3965| Corona            | USA  |  California  |124966 
-3973| Concord           | USA  |  California  |121780 
-3977| Cedar Rapids      | USA  |  Iowa        |120758 
-3982| Coral Springs     | USA  |  Florida     |117549 
+id  |   Name		|CityCode|  District	|Population
+3878| Scottsdale        | USA    |  Arizona     | 202705 
+3965| Corona            | USA    |  California  | 124966 
+3973| Concord           | USA    |  California  | 121780 
+3977| Cedar Rapids      | USA    |  Iowa        | 120758 
+3982| Coral Springs     | USA    |  Florida     | 117549 
 
 --question
 --Query all columns for all American cities in the CITY table with populations larger than 100000. The CountryCode for America is USA.
