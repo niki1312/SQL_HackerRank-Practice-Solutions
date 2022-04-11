901def34111f97c7bdfe20678290cc6f706a764e
Nikita Makarand Pande <53806378+niki1312@users.noreply.github.com>
2022-04-11 11:50:21 +0530

    Create Q&A SqlStationDataset.sql

---
+++ Q&A SqlStationDataset.sql
@@ -0,0 +1,26 @@
+
+---q1.Query a list of CITY and STATE from the STATION table.
+--solution:
+
+select city,state from station 
+
+Acme LA 
+Addison MI 
+Agency MO 
+Aguanga CA 
+Alanson MI 
+Alba MI 
+Albany CA 
+Albion IN 
+Algonac MI 
+Aliso Viejo CA 
+Allerton IA 
+Alpine AR 
+Alton MO 
+Amazonia MO 
+Amo IN 
+Andersonville GA 
+Andover CT 
+Anthony KS 
+Archie MO 
+Arispe IA {-truncated-}
